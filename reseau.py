1acb448869e10500a5decbf4b68d515e04fc9da3
CascadeDeTorche <64471244+CascadeDeTorche@users.noreply.github.com>
2021-05-28 15:10:47 +0200

    On crée le model

--- reseau.py
+++ reseau.py
@@ -18,7 +18,7 @@ from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 from keras.optimizers import SGD
-
+from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 import tensorflow as tf
 from keras.preprocessing import image
@@ -54,92 +54,18 @@ print('total validation images cruciforme:', len(os.listdir(validation_cruciform
 print('total validation images plat:', len(os.listdir(validation_plat_dir)))
 
 
-# datagen = ImageDataGenerator(rotation_range=40,      # rotation de l'image originale
-#                              width_shift_range=0.2,  # Translation horizontale et verticale de l'image
-#                              height_shift_range=0.2,
-#                              shear_range=0.2,        # Cisaillement aléatoire
-#                              zoom_range=0.2,         # Zoom aléatoire
-#                              horizontal_flip=True,   # Rotation à 90° de la moitié des données aléatoirement
-#                              fill_mode='nearest')    # Stratégie à adopter pour le remplissage des pixels créés
+train_datagen = ImageDataGenerator(rescale=1/.255)
+validation_datagen = ImageDataGenerator(rescale=1/.255)
 
-# fnames = [os.path.join(train_cruciforme_dir, fname) for fname in os.listdir(train_cruciforme_dir)]
+train_generator = train_datagen.flow_from_directory(train_dir, target_size=(816,612),class_mode="categorical")
 
+validation_generator = train_datagen.flow_from_directory(validation_dir, target_size=(816,612),class_mode="categorical")
 
-# img_path = fnames[3] # on choisit d'augmenter une image 
-# img = image.load_img(img_path, target_size=(3264, 2448))
+# Creation du model
 
-
-
-# représenttaion d'un exemple d'augmentation d'image
-
-# x = image.img_to_array(img)
-# x = x.reshape((1,) + x.shape)
-# i = 0
-# for batch in datagen.flow(x, batch_size=1):
-#     plt.figure(i)
-#     imgplot = plt.imshow(image.array_to_img(batch[0]))
-#     i += 1
-#     if i % 4 == 0:
-#         break          # le générateur tourne en boucle sur lui même, il faut indiquer une condition d'arrêt
-# plt.show()
+model=Sequential()
 
 
 
 
 
-
-
-
-# # définition du modèle
-
-# model = Sequential()
-
-# # première couche de convolution
-# model.add(Conv2D(32, (3, 3), activation='relu',
-#           input_shape=(150, 150, 3)))
-
-# #première couche de pooling
-# model.add(MaxPooling2D((2, 2)))
-
-# # deuxième couche de convolution
-# model.add(Conv2D(64, (3, 3), activation='relu'))
-
-# # deuxième couche de pooling
-# model.add(MaxPooling2D((2, 2)))
-# # ...
-# model.add(Conv2D(128, (3, 3), activation='relu'))
-# #...
-# model.add(MaxPooling2D((2, 2)))
-# #...
-# model.add(Conv2D(128, (3, 3), activation='relu'))
-
-# model.add(MaxPooling2D((2, 2)))
-
-# # vectorisation de l'image résultante
-# model.add(Flatten())
-
-# model.add(Dense(512, activation='relu'))
-
-# # Classification binaire 
-# model.add(Dense(1, activation='sigmoid'))
-
-# from keras import optimizers
-
-# model.compile(loss='binary_crossentropy',
-#               optimizer=optimizers.RMSprop(lr=1e-4),
-#               metrics=['acc'])
-
-# from keras.preprocessing.image import ImageDataGenerator
-
-# train_datagen = ImageDataGenerator(rescale=1./255)
-# validation_datagen = ImageDataGenerator(rescale=1./255)
-
-# train_generator = train_datagen.flow_from_directory(train_dir,               # On précise la localisation des images
-#                                                     target_size=(150, 150),  # redimensionnement en 150*150
-#                                                     batch_size=20,           # Batchs de 20 images 
-#                                                     class_mode='binary')     # labels binaires 
-
-# validation_generator = validation_datagen.flow_from_directory(validation_dir,
-#                                                         target_size=(150, 150),
-#                                                         batch_size=20,
-#                                                         class_mode='binary')
