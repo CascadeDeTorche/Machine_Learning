80c8800a50882f80674086dcdbbdc32a83720438
Hrokdulf <hrokdulf.ironstone@gmail.com>
2021-06-02 18:57:41 +0200

    Update reseau_crop.py

--- reseau_crop.py
+++ reseau_crop.py
@@ -99,26 +99,23 @@ normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_ds))
 
-# Creation du model
+model = Sequential([
+  layers.Conv2D(16, 3, padding='same', activation='relu',input_shape=(img_height,img_width,3)),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(4,activation='softmax')
+])
 
-model=Sequential()
 
-#Première Convolution
-model.add(Conv2D(16,(5,5),activation='relu',input_shape=(img_height,img_width,3)))
-model.add(MaxPooling2D((2,2)))
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 
-#Deuxième Convolution
-model.add(Conv2D(32,(3,3),activation='relu'))
-model.add(MaxPooling2D((2,2)))
-
-#Vectorisation des images
-model.add(Flatten())
-
-#Reseau de neuronne 
-model.add(Dense(512,activation = 'relu'))
-model.add(Dense(4,activation = 'softmax'))
-
-model.compile(optimizer='rmsprop',loss='categorical_crossentropy',metrics=['accuracy'])
 
 #je conseil de lancer ça dans le terminal après le script
 
